0f1195331de9847a3d5c908455614638b0362bec
glen1986 <escobar.glen@gmail.com>
2021-10-14 15:43:08 -0300

    pages

--- config_web_com.php
+++ config_web_com.php
@@ -12,7 +12,7 @@
 	spl_autoload_register($autoload);
 
 
-	define('INCLUDE_PATH','https://omundodadarthi.com.br/');
+	define('INCLUDE_PATH','http://omundodadarthi.com.br/');
 	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
 
   define('BASE_DIR_PAINEL',__DIR__.'/painel/');
